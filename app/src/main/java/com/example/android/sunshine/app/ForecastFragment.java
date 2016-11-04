9bec4bc9300e74925eac97a3d386c3f226f7d6fd
Calvin Christian <calvinchrst@gmail.com>
2016-11-04 09:42:26 +0700

    Fix Typo in forecast base URL.

--- app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastFragment.java
@@ -240,7 +240,7 @@ public class ForecastFragment extends Fragment {
                 // Construct the URL for the OpenWeatherMap query
                 // Possible parameters are available at OWM's forecast API page, at
                 // http://openweathermap.org/API#forecast
-                final String FORECAST_BASE_URL ="http://api.openweathermap.org/data/2.5/forecast/daily?";
+                final String FORECAST_BASE_URL ="http://api.openweathermap.org/data/2.5/forecast/daily";
                 final String QUERY_PARAM = "q";
                 final String FORMAT_PARAM = "mode";
                 final String UNITS_PARAM = "units";
@@ -248,7 +248,7 @@ public class ForecastFragment extends Fragment {
                 final String APPID_PARAM = "APPID";
 
                 Uri.Builder uri = new Uri.Builder();
-                uri.encodedPath("http://api.openweathermap.org/data/2.5/forecast/daily")
+                uri.encodedPath(FORECAST_BASE_URL)
                         .appendQueryParameter(QUERY_PARAM, params[0])
                         .appendQueryParameter(DAYS_PARAM, Integer.toString(numDays))
                         .appendQueryParameter(UNITS_PARAM, weatherUnits)
